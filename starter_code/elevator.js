0e5c955cf5c61e79575f9abb85c52c5a4fc5555c
SarahNox <apnoeist@googlemail.com>
2017-02-08 08:45:24 +0100

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -2,18 +2,71 @@ class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
+    this.timer;
     this.requests   = [];
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.timer = setInterval(this.update.bind(this), 1000);
+  }
+  stop() {
+    clearInterval(this.timer);
+  }
+  update() {
+    console.log(this.log());
+    this._passengersEnter();
+    this._passengersLeave();
+    for (let i = 0; i < this.requests.length; i++) {
+      if (this.floor === this.requests[i]) {
+        this.requests.splice(i, 1);
+      }
+    }
+    if (this.requests[0] > this.floor) {
+      this.floorUp();
+    } else if (this.requests[0] < this.floor) {
+      this.floorDown();
+    }
+    if (this.requests.length === 0) {
+      this.stop();
+    }
+  }
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+     this.floor ++;
+   }
+  }
+  floorDown() {
+    if (this.floor > 0) {
+     this.floor --;
+   }
+  }
+  _passengersEnter() {
+    for (let i = 0; i < this.waitingList.length; i++) {
+      if (this.floor === this.waitingList[i].originFloor) {
+        this.passengers.push(this.waitingList[i]);
+        console.log(`${this.waitingList[i].name} has entered the elevator.`);
+        this.requests.push(this.waitingList[i].destinationFloor)
+        this.waitingList.splice(i, 1);
+      }
+    }
+  }
+  _passengersLeave() {
+    for (let i = 0; i < this.passengers.length; i++) {
+      if (this.floor === this.passengers[i].destinationFloor) {
+        console.log(`${this.passengers[i].name} has left the elevator.`);
+        this.passengers.splice(i, 1);
+      }
+    }
+  }
+  call(person) {
+    this.waitingList.push(person)
+    this.requests.push(person.originFloor);
+  }
+  log() {
+    return `Current floor: ${this.floor}`;
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,14 @@
-const Elevator = require('./elevator.js');
+const Elevator = require('./elevator');
+const Person = require('./person');
+
+let elevator = new Elevator();
+let daniel = new Person("Daniel", 0, 5);
+let laura = new Person("Laura", 2, 10);
+let sebastian = new Person("Sebastian", 7, 1);
+
+elevator.start();
+elevator.call(daniel);
+// elevator.call(laura);
+elevator.call(sebastian);
+
+setTimeout(() => {elevator.call(laura)}, 6000);
